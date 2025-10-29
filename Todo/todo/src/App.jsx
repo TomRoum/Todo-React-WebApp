dd2593a19980ec16840fc8ad8af150951c1df143
TomRoum <tomi.roumio@gmail.com>
2025-10-29 17:27:56 +0200

    Todo started

---
+++ Todo/todo/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div id="container">
+      <h3>Todos</h3>
+      <form>
+        <input placeholder="Add new task" />
+      </form>
+      <ul></ul>
+    </div>
+  );
+}
+
+export default App;
