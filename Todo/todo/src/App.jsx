749d304df64e74d93a0d2b766fb48f18eaf074cc
TomRoum <tomi.roumio@gmail.com>
2025-10-29 18:45:28 +0200

    todo 1 done

--- Todo/todo/src/App.jsx
+++ Todo/todo/src/App.jsx
@@ -1,13 +1,49 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  
+  const addTask = () => {
+    if (task.trim()) {
+      setTasks([...tasks, task]);
+      setTask("");
+    }
+  };
+  
+  const deleteTask = (taskToDelete) => {
+    setTasks(tasks.filter(item => item !== taskToDelete));
+  };
+  
   return (
     <div id="container">
-      <h3>Todos</h3>
-      <form>
-        <input placeholder="Add new task" />
-      </form>
-      <ul></ul>
+      <div className="card">
+        <h3>Todos</h3>
+        <div>
+          <input
+            placeholder="Add new task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault();
+                addTask();
+              }
+            }}
+          />
+        </div>
+        <ul>
+          {tasks.map((item, index) => (
+            <li key={index}>
+              <span>{item}</span>
+              <button className="delete-button" onClick={() => deleteTask(item)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
